6c12432f43e757c3479498dc0901096b04ee8c6b
IceCereal <ns_raghav@hotmail.com>
2020-01-28 23:17:01 +0530

    Add Lab-2

---
+++ 2-Jan-22/Task-1/server.py
@@ -0,0 +1,87 @@
+import socket
+from random import randint
+from ast import literal_eval as l_e
+from time import sleep
+
+# The socket Instance
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+# Local Machine Name
+host = socket.gethostbyname(socket.gethostname())
+
+# Choose port & bind it
+port = 3011
+s.bind((host, port))
+
+print ("host ip:port:\t", host, port)
+
+# Listen for client connections. The number
+# it specifies the number of unaccepted connections that the system
+# will allow before refusing newconnections.
+s.listen(5)
+
+while True:
+	# Establish a connection with the client
+	cli, addr = s.accept()
+
+	# Yay!
+	print ("Got connection from", addr)
+
+	# Initial Prompt
+	"""
+		Type of Service:
+			0 - deduplication + [___]
+			1 - intersection + [1___]
+	"""
+	prompt = int(cli.recv(1024).decode("utf-8"))
+
+	if prompt <= 999:
+		Client_List = cli.recv(1024).decode("utf-8")
+		List = l_e(Client_List)
+		ListDup = [i for i in List]
+
+		print ("clientList (unsorted):\t", List)
+
+		serverList = [randint(1, 100) for i in range(prompt)]
+
+		List.sort()
+
+		ListDup = set(ListDup)
+		serverList = set(ListDup)
+
+		commonNumbers = []
+
+		for i in ListDup:
+			if i in serverList:
+				commonNumbers.append(i)
+
+		cli.send(bytes(str(List).encode("utf-8")))
+		sleep(0.25)
+
+		print ("serverList:\t", list(serverList))
+		print ("clientList (sorted):\t", List)
+		print ("commonList:\t", commonNumbers)
+
+		cli.send(bytes(str(commonNumbers).encode("utf-8")))
+	else:
+		Client_List = cli.recv(1024).decode("utf-8")
+		List = l_e(Client_List)
+
+		print ("clientList (unsorted):\t", List)
+
+		len_list = prompt - 1000
+
+		serverList = [randint(1, 100) for i in range(len_list)]
+
+		print ("serverList:\t", serverList)
+
+		List = set(List)
+		serverList = set(List)
+
+		intersectedList = list(List.intersection(serverList))
+
+		print ("intersectedList", intersectedList)
+
+		cli.send(bytes(str(intersectedList).encode("utf-8")))
+
+	cli.close()
---
+++ 2-Jan-22/Task-3/client.py
@@ -0,0 +1,64 @@
+import socket
+from random import randint
+from ast import literal_eval as le
+from time import sleep
+
+# The socket Instance
+s = socket.socket()
+
+# Local Machine Name
+host = socket.gethostname()
+
+# Server Port
+portServer = 3010
+
+# Client Port (for incoming connections)
+portClient = 1337
+
+s.bind((host, portClient))
+s.connect(("10.59.102.66", portServer))
+
+N = 80 # Number of elements (Max: 999)
+if N > 999:
+	print ("N cannot exceed 999...\nExiting")
+	exit()
+
+"""
+	Type of Service:
+		0 - deduplication
+		1 - intersection
+"""
+service = 1
+
+if service == 0:
+	s.send(bytes(str(1).encode("utf-8")))
+	sleep(0.25)
+
+	array = [randint(1, 100) for i in range(N)]
+
+	s.send(bytes(str(array).encode("utf-8")))
+
+	dedup_list_raw = s.recv(1024).decode("utf-8")
+	dedup_list = le(dedup_list_raw)
+
+	print ("Deduplicated List:\t", dedup_list)
+
+	commonNumbers_raw= s.recv(1024).decode("utf-8")
+	commonNumbers = le(commonNumbers_raw)
+
+	print ("Common Numbers:\t", commonNumbers)
+
+elif service == 1:
+	s.send(bytes(str(2).encode("utf-8")))
+	sleep(0.25)
+
+	array = [randint(1, 100) for i in range(N)]
+
+	s.send(bytes(str(array).encode("utf-8")))
+
+	intersected_list_raw = s.recv(1024).decode("utf-8")
+	intersected_list = le(intersected_list_raw)
+
+	print ("Intersected List:\t", intersected_list)
+
+s.close()
